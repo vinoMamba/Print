ffb91cc0116b847d77b4a3651f21478149ddbbd0
vino <vino0908@outlook.com>
2022-05-26 15:18:13 +0800

    feat: 打印页面调整

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ function print() {
   createApp({
     render() {
       const list = []
-      for (let i = 0; i < 1000; i++) {
+      for (let i = 0; i < 10; i++) {
         const label = h(Label, {
           props: {
             title: `hello ${i}`
@@ -47,9 +47,11 @@ const printObj = ref(
 
 <template>
   <main>
-    <button v-print="printObj" @click="print">打印</button>
-    <button @click="close">关闭</button>
-    <div ref="containerRef" class="container">
+    <img src="./assets/print.png" alt="">
+    <p>请使用Chrome、360、QQ、搜狗等浏览器，以获得最佳打印效果</p>
+    <div class="button-wrapper">
+      <button v-print="printObj" @click="print">打印</button>
+      <button @click="close">关闭</button>
     </div>
   </main>
 </template>
@@ -62,7 +64,6 @@ const printObj = ref(
 }
 
 body {
-  border: 1px solid red;
   height: 100vh;
   width: 100vw;
   overflow: hidden;
@@ -70,8 +71,35 @@ body {
 </style>
 <style scoped>
 main {
-
+  font-family: -apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
+  background-color: #eff0f4;
+  padding-top: 100px;
   height: 100vh;
   width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+p {
+  font-size: 20px;
+}
+
+.button-wrapper {
+  margin-top: 16px;
+  display: flex;
+  justify-content: space-around;
+}
+
+button {
+  border-radius: 4px;
+  font-size: 16px;
+  margin: 4px;
+  padding: 4px 32px;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  color: white;
+  background-color: #48a5ff;
 }
 </style>
